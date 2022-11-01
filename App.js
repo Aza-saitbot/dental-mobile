8f6b984bf19c57e0c58493a555879b14409ec0f1
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2022-11-01 09:22:41 +0300

    feat/added style

--- App.js
+++ App.js
@@ -1,47 +1,80 @@
 import styled from 'styled-components/native'
-import Group from './components/Group'
+import {Ionicons} from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { Appointment, SectionTitle } from './components'
+import SectionList from 'react-native/Libraries/Lists/SectionList'
 
-const items=[
+const data = [
     {
-        id:1,
-        active:true,
-        image:'https://upload.wikimedia.org/wikipedia/commons/a/af/Dwayne_Johnson_2014%28Cropped%29.jpg',
-        fullname:'Дуэйн Джонсон',
-        diagnosis:'лечение зубов, легкий кариес',
-        date:'11:30'
+        active: true, diagnosis: 'лечение зубов, легкий кариес',
+        time: '11:30', user:{
+            image: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Dwayne_Johnson_2014%28Cropped%29.jpg',
+            fullname: 'Дуэйн Джонсон',
+        }
     },
     {
-        id:2,
-        active:false,
-        image:'https://s2.vcdn.biz/static/f/2326760021/image.jpg',
-        fullname:'Джеки Чан',
-        diagnosis:'удаление зубов',
-        date:'13:30'
+        active: false, diagnosis: 'удаление зубов',
+        time: '14:30', user:{
+            image: 'https://s2.vcdn.biz/static/f/2326760021/image.jpg',
+            fullname: 'Джеки Чан',
+        }
     },
+
 ]
 
-const mockData=[
-    {id:1, title:'30 октября',items},
-    {id:2, title:'1 ноября',items},
+const DATA = [
+    {title: '30 октября', data},
+    {title: '1 ноября', data},
+    {title: '1 ноября', data},
+    {title: '1 ноября', data},
+    {title: '1 ноября', data},
+    {title: '1 ноября', data},
 ]
 
 export default function App() {
     return (
         <Container>
-            {mockData.map(i=>
-                <Group
-                    key={i.id}
-                    title={i.title}
-                    items={i.items}
-                />
-            )}
+            <SectionList
+                sections={DATA}
+                keyExtractor={(item, index) => index}
+                renderItem={({item}) => <Appointment {...item} />}
+                renderSectionHeader={({section: {title}}) => (
+                    <SectionTitle>{title}</SectionTitle>
+                )}
+            />
+            <PlusButton style={{
+                shadowColor: "#2a86ff",
+                shadowOffset: {
+                    width: 0,
+                    height: 2,
+                },
+                shadowOpacity: 0.7,
+                shadowRadius: 3.5,
+                elevation: 4,
+            }}>
+                <AntDesign name="plus" size={36} color="white" />
+            </PlusButton>
         </Container>
     )
 }
 
+
+const PlusButton=styled.TouchableOpacity`
+align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+  width: 64px;
+  height: 64px;
+  background: #2a86ff;
+  position: absolute;
+  right: 25px;
+  bottom: 25px;
+`
+
 const Container = styled.View`
   flex: 1;
-  margin-top: 50px;
+  margin-top: 30px;
+  padding: 0 20px;
 `
 
 
---
+++ components/Appointment.js
@@ -0,0 +1,55 @@
+import { View } from 'react-native'
+import styled from 'styled-components/native'
+
+export default function Appointment({user,diagnosis,active,time}) {
+    return (
+        <GroupItem>
+            <Avatar source={{
+                uri: user.image
+            }}/>
+            <View style={{flex: 1}}>
+                <FullName>{user.fullname}</FullName>
+                <GrayText>{diagnosis}</GrayText>
+            </View>
+            <GroupDate active={active}>{time}</GroupDate>
+        </GroupItem>
+    )
+}
+
+
+const GroupDate = styled.Text`
+  background: ${props => props.active ? '#2A86FF' : '#e9f5ff'};
+  color: ${props => props.active ? '#FFF' : '#4294ff'};
+  border-radius: 18px;
+  font-weight: 600;
+  font-size: 14px;
+  width: 70px;
+  text-align: center;
+  line-height: 28px;
+`
+
+const GrayText = styled.Text`
+  font-size: 16px;
+  color: #8b979f;
+`
+
+const FullName = styled.Text`
+  font-weight: 600;
+  font-size: 16px;
+`
+
+const Avatar = styled.Image`
+  border-radius: 50px;
+  width: 40px;
+  height: 40px;
+  margin-right: 15px;
+`
+
+const GroupItem = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  padding: 20px;
+  border-bottom-color: #f3f3f3;
+  border-bottom-width: 1px;
+`
+
